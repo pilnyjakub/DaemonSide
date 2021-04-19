416d9b419294d88f517c84e509b46a41ff8cf17a
pilnyjakub <jakubpil@seznam.cz>
2021-04-19 18:16:55 +0200

    init

---
+++ DaemonSide/Tables.cs
@@ -0,0 +1,62 @@
+﻿namespace DaemonSide
+{
+    class PcBackup
+    {
+        public int Id { get; set; }
+        public int IdPc { get; set; }
+        public int IdConfig { get; set; }
+    }
+    class BackupFiles
+    {
+        public int Id { get; set; }
+        public int IdConfig { get; set; }
+        public string Path { get; set; }
+    }
+    class Time
+    {
+        public int Id { get; set; }
+        public int IdConfig { get; set; }
+        public string Frequency { get; set; }
+        public string Days { get; set; }
+        public string Date { get; set; }
+    }
+    public class Storage
+    {
+        public int Id { get; set; }
+        public int IdConfig { get; set; }
+        public string Place { get; set; }
+        public string ServerIP { get; set; }
+        public string ServerLogin { get; set; }
+        public string ServerPass { get; set; }
+        public string Path { get; set; }
+    }
+    public class BackupConfig
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Email { get; set; }
+        public bool EmailBool { get; set; }
+    }
+    public class Backup
+    {
+        public int Id { get; set; }
+        public string BackupTime { get; set; }
+        public int FileCount { get; set; }
+        public int FileCountSuccess { get; set; }
+        public int FileCountFailed { get; set; }
+        public string Errors { get; set; }
+        public int IdPcBackUp { get; set; }
+        public string Operations { get; set; }
+    }
+    public class Pc
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IpAddress { get; set; }
+        public string MacAddress { get; set; }
+        public string OS { get; set; }
+        public string State { get; set; }
+        private Pc() { }
+        public static Pc Instance = new Pc();
+    }
+}
